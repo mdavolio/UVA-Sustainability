dc935f643288461c981e396ddf95e51e0a0c9132
Matt <md3es@virginia.edu>
2016-12-04 15:41:53 -0500

    read in data

--- Rice_Data.R
+++ Rice_Data.R
@@ -2,3 +2,9 @@
 
 library(readxl)
 
+rice_e <- read_excel('Rice Hall 0214.xlsx', sheet = 1) # Rice Hall Electricity
+
+rice_hw <- read_excel('Rice Hall 0214.xlsx', sheet = 2) # Rice Hall Hot Water
+
+rice_cw <- read_excel('Rice Hall 0214.xlsx', sheet = 3) # Rice Hall Chilled Water
+
