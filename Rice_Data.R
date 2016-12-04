8dd2e35cdd522e364ce89e117a4b9e6277fe6961
Matt <md3es@virginia.edu>
2016-12-04 16:08:51 -0500

    function attempt

--- Rice_Data.R
+++ Rice_Data.R
@@ -2,9 +2,32 @@
 
 library(readxl)
 
-rice_e <- read_excel('Rice Hall 0214.xlsx', sheet = 1) # Rice Hall Electricity
+electric <- read_excel('Rice Hall 0214.xlsx', sheet = 1) # Rice Hall Electricity
 
 rice_hw <- read_excel('Rice Hall 0214.xlsx', sheet = 2) # Rice Hall Hot Water
 
 rice_cw <- read_excel('Rice Hall 0214.xlsx', sheet = 3) # Rice Hall Chilled Water
 
+
+
+
+
+"""
+read_build <- function(building, path){
+  library(readxl)
+  
+  electric <- read_excel(path, sheet = 1)
+  assign(paste(building, 'e', sep='_'), electric)
+  return(paste(building, 'e', sep='_'))
+  
+  hot <- read_excel(path, sheet = 2)
+  assign(paste(building, 'hw', sep='_'), hot)
+  return(paste(building, 'hw', sep='_'))
+  
+  chilled <- read_excel(path, sheet = 1)
+  assign(paste(building, 'cw', sep='_'), chilled)
+  return(paste(building, 'cw', sep='_'))
+}
+
+read_build('Rice', 'Rice Hall 0214.xlsx')
+"""
