f9be66b18fb46d9b052cdaaa51f92adc4eae1b01
Matt <md3es@virginia.edu>
2016-12-04 16:30:52 -0500

    rename columns

--- Rice_Data.R
+++ Rice_Data.R
@@ -21,9 +21,18 @@ rice_e <- rice_e[, names(rice_e) %in% keep]
 rice_hw <- rice_hw[, names(rice_hw) %in% keep]
 rice_cw <- rice_cw[, names(rice_cw) %in% keep]
 
+# rename columns for ease
+oldnames_e <- names(rice_e)
+newnames_e <- c('Timestamp','Min_e','Min_Time_e','Max_e','Max_Time_e','Avg_e','Interpolative_e')
+setnames(rice_e, oldnames_e, newnames_e)
 
+oldnames_hw <- names(rice_hw)
+newnames_hw <- c('Timestamp','Min_hw','Min_Time_hw','Max_hw','Max_Time_hw','Avg_hw','Interpolative_hw')
+setnames(rice_hw, oldnames_hw, newnames_hw)
 
-
+oldnames_cw <- names(rice_cw)
+newnames_cw <- c('Timestamp','Min_cw','Min_Time_cw','Max_cw','Max_Time_cw','Avg_cw','Interpolative_cw')
+setnames(rice_cw, oldnames_cw, newnames_cw)
 
 """
 read_build <- function(building, path){
