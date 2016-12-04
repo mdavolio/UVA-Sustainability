d8c321635d26527c034c774523ae89a0adc43996
Matt <md3es@virginia.edu>
2016-12-04 16:12:45 -0500

    remove imcomplete obs

--- Rice_Data.R
+++ Rice_Data.R
@@ -2,15 +2,15 @@
 
 library(readxl)
 
-electric <- read_excel('Rice Hall 0214.xlsx', sheet = 1) # Rice Hall Electricity
-
+# Read in Files
+rice_e <- read_excel('Rice Hall 0214.xlsx', sheet = 1) # Rice Hall Electricity
 rice_hw <- read_excel('Rice Hall 0214.xlsx', sheet = 2) # Rice Hall Hot Water
-
 rice_cw <- read_excel('Rice Hall 0214.xlsx', sheet = 3) # Rice Hall Chilled Water
 
-
-
-
+# Remove incomplete observations
+rice_e <- rice_e[complete.cases(rice_e),]
+rice_hw <- rice_hw[complete.cases(rice_hw),]
+rice_cw <- rice_cw[complete.cases(rice_cw),]
 
 """
 read_build <- function(building, path){
