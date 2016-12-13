7225b76288b7b870d1b1cee299f1194f51a833f6
Tyler <tch6zf@virginia.edu>
2016-12-13 15:37:58 -0500

    read in monthly billing data and perform some cleaning

--- monthly_read.R
+++ monthly_read.R
@@ -14,3 +14,20 @@ suppressPackageStartupMessages({
 
 # No scientific notation
 options(scipen=999)
+
+# Read in monthly billing data and perform data type corrections, mutations, and selections
+monthly.bills <- read_excel('Building Billing History - West District.xlsx', col_names = TRUE) %>% 
+  select(c(2:10,14)) %>% 
+  mutate(BillDate = as.Date(format(BillMonth,"%Y-%m-%d"))) %>% 
+  mutate(Month = factor(month(BillDate, label = FALSE))) %>% 
+  mutate(Year = factor(year(BillDate))) %>% 
+  select(-c(1)) %>% 
+  mutate(Building = factor(Building)) %>% 
+  mutate(MeterID = factor(MeterID)) %>% 
+  mutate(PlantID = factor(PlantID)) %>% 
+  mutate(Product = factor(Product)) %>% 
+  mutate(Units = factor(Units)) %>% 
+  select(-c(10)) %>% 
+  select(c(11,10,1:9))
+
+
