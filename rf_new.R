0235a7911befe55808a6811699728803bff6575b
Tyler <tch6zf@virginia.edu>
2016-12-13 21:55:09 -0500

    more stuff

--- rf_new.R
+++ rf_new.R
@@ -4,18 +4,22 @@
 
 suppressPackageStartupMessages({
   library(caret)
-  library(ranger)
   library(e1071)
   library(randomForest)
+  library(leaps)
+  library(car)
 })
 
-# 5-fold cross validation, repeated 5 times
+
+### Random Forest
+
+# 10-fold cross validation, repeated 10 times
 cvControl <- trainControl(method = "repeatedCV",
                           repeats = 10,
                           number = 10)
 
 # Run random forest
-rf.mod <- train(MTeCO2 ~ nBuild + total_cost + nPlants + sqft + session,
+rf.mod <- train(MTeCO2 ~ nBuild + nPlants + sqft + session + Year + Month + AvgTemp + AvgHum,
                   data = footprint,
                   method = 'rf',
                   ntree = 850,
@@ -25,13 +29,39 @@ rf.mod <- train(MTeCO2 ~ nBuild + total_cost + nPlants + sqft + session,
 
 # Create Time slices
 timeControl <- trainControl(method = 'timeslice',
-                            initialWindow = 3,
-                            horizon = 3,
+                            initialWindow = 12,
+                            horizon = 6,
                             fixedWindow = T)
 
 # training model
-rf.mod_ts <- train(MTeCO2 ~ nBuild + total_cost + nPlants + sqft + session,
+rf.mod_ts <- train(MTeCO2 ~ nBuild +  nPlants + sqft + session + Year + Month + AvgTemp + AvgHum,
                 data = footprint,
                 method = 'rf',
                 ntree = 850,
                 trControl = timeControl)
+
+
+### Linear Model
+
+# 10-fold cross validation, repeated 10 times
+cvControl <- trainControl(method = "LOOCV")
+
+# Run random forest
+lm.mod <- train(MTeCO2 ~ nBuild +  nPlants + sqft + session + AvgTemp + AvgHum,
+                data = footprint,
+                method = 'lm',
+                trControl = cvControl)
+
+# Time Series Random Forest
+
+# Create Time slices
+timeControl <- trainControl(method = 'timeslice',
+                            initialWindow = 12,
+                            horizon = 6,
+                            fixedWindow = T)
+
+# training model
+lm.mod_ts <- train(MTeCO2 ~ nBuild +  nPlants + sqft + session + AvgTemp + AvgHum,
+                   data = footprint,
+                   method = 'lm',
+                   trControl = timeControl)
