bd3ce015b65a162b88b1d6d240cad9e9fb81b18f
kjones8812 <kjones8812@gmail.com>
2016-12-13 21:54:15 -0500

    prep

--- weather.R
+++ weather.R
@@ -7,15 +7,20 @@ weather3 <- read.csv("weather_3.csv")
 weather<-rbind(weather1,weather2)
 weather<-rbind(weather,weather3)
 
+
 #Convert date
 weather$Month<-month(as.Date(weather$EDT))
 weather$Year<-year(as.Date(weather$EDT))
+weather$PrecipitationIn<-as.numeric(as.character(weather$PrecipitationIn))
+
 
 #find aveage humidity/temperature
 Avg_Weather<- weather %>%
   group_by(Year,Month) %>%
   summarise(AvgTemp = mean(Mean.TemperatureF),
-            AvgHum = mean(Mean.Humidity)) 
+            AvgHum = mean(Mean.Humidity),
+            sumPrep = sum(PrecipitationIn, na.rm = TRUE))
+
 #order by year, month
 Avg_Weather<-Avg_Weather[order(Avg_Weather$Year,Avg_Weather$Month),]
 
