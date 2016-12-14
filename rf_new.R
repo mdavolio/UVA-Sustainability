4558c4c85f1f75a3e60025259e784db23df74cdd
Matt <md3es@virginia.edu>
2016-12-13 20:02:53 -0500

    time series cv

--- rf_new.R
+++ rf_new.R
@@ -11,13 +11,27 @@ suppressPackageStartupMessages({
 
 # 5-fold cross validation, repeated 5 times
 cvControl <- trainControl(method = "repeatedCV",
-                          repeats = 5,
-                          number = 5)
+                          repeats = 10,
+                          number = 10)
 
-# Run random forrest
+# Run random forest
 rf.mod <- train(MTeCO2 ~ nBuild + total_cost + nPlants + sqft + session,
                   data = footprint,
                   method = 'rf',
+                  ntree = 850,
                   trControl = cvControl)
 
+# Time Series Random Forest
 
+# Create Time slices
+timeControl <- trainControl(method = 'timeslice',
+                            initialWindow = 3,
+                            horizon = 3,
+                            fixedWindow = T)
+
+# training model
+rf.mod_ts <- train(MTeCO2 ~ nBuild + total_cost + nPlants + sqft + session,
+                data = footprint,
+                method = 'rf',
+                ntree = 850,
+                trControl = timeControl)
