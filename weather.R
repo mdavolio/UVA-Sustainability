df28d07b4c4ee91b020d9a3b0b39d160d9ffaf8f
kjones8812 <kjones8812@gmail.com>
2016-12-13 20:47:27 -0500

    avg weather

---
+++ weather.R
@@ -0,0 +1,22 @@
+library(dplyr)
+library(lubridate)
+weather1 <- read.csv("weather_1.csv")
+weather2 <- read.csv("weather_2.csv")
+weather3 <- read.csv("weather_3.csv")
+
+weather<-rbind(weather1,weather2)
+weather<-rbind(weather,weather3)
+
+#Convert date
+weather$Month<-month(as.Date(weather$EDT))
+weather$Year<-year(as.Date(weather$EDT))
+
+#find aveage humidity/temperature
+Avg_Weather<- weather %>%
+  group_by(Year,Month) %>%
+  summarise(AvgTemp = mean(Mean.TemperatureF),
+            AvgHum = mean(Mean.Humidity)) 
+#order by year, month
+Avg_Weather<-Avg_Weather[order(Avg_Weather$Year,Avg_Weather$Month),]
+
+write.csv(Avg_Weather,"Avg_Weather.csv")
